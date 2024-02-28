f3ad956b380422f61bb2cdbe4da6c49a62a0c58a
cyd losekann <cydnel@gmail.com>
2024-02-28 06:35:14 -0300

    identacao ok

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import face_recognition
 
 imagem_referencia = face_recognition.load_image_file("pessoas/rosto1.jpg")
-
 codificacao_referencia = face_recognition.face_encodings(imagem_referencia)[0]
-
 imagem_desconhecida = face_recognition.load_image_file("pessoas/rosto2.jpg")
-
 codificacao_desconhecida = face_recognition.face_encodings(imagem_desconhecida)[0]
-
 resultado = face_recognition.compare_faces([codificacao_referencia], codificacao_desconhecida)
 
 print('As faces são iguais? ', resultado[0])
