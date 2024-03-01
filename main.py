e30974212b9daccadca3cc2a15a32ae87df6336a
cyd losekann <cydnel@gmail.com>
2024-03-01 00:46:41 -0300

    tratamento de erros add

--- main.py
+++ main.py
@@ -8,24 +8,30 @@ def selecionar_arquivo(titulo="Selecione um arquivo", tipo_de_arquivo=(("Imagens
 
 def comparar_faces(imagem_referencia_path, imagem_desconhecida_path):
     imagem_referencia = face_recognition.load_image_file(imagem_referencia_path)
-    codificacao_referencia = face_recognition.face_encodings(imagem_referencia)[0]
-
+    codificacoes_referencia = face_recognition.face_encodings(imagem_referencia)
     imagem_desconhecida = face_recognition.load_image_file(imagem_desconhecida_path)
-    codificacao_desconhecida = face_recognition.face_encodings(imagem_desconhecida)[0]
+    codificacoes_desconhecida = face_recognition.face_encodings(imagem_desconhecida)
 
-    resultado = face_recognition.compare_faces([codificacao_referencia], codificacao_desconhecida)
-    print('As faces são iguais? ', resultado[0])
+    if codificacoes_referencia and codificacoes_desconhecida:
+        resultado = face_recognition.compare_faces([codificacoes_referencia[0]], codificacoes_desconhecida[0])
+        return resultado[0]
+    else:
+        return None
 
 def iniciar_comparacao():
     imagem_referencia_path = selecionar_arquivo("Selecione a imagem de referência")
     imagem_desconhecida_path = selecionar_arquivo("Selecione a imagem desconhecida")
 
     if imagem_referencia_path and imagem_desconhecida_path:
-        comparar_faces(imagem_referencia_path, imagem_desconhecida_path)
+        resultado = comparar_faces(imagem_referencia_path, imagem_desconhecida_path)
+        if resultado is None:
+            print("Não foi possível encontrar faces em uma das imagens.")
+        else:
+            print('As faces são iguais? ', resultado)
     else:
         print("Seleção de arquivo cancelada.")
 
-
 raiz = tk.Tk()
-raiz.withdraw() 
+raiz.withdraw()
 iniciar_comparacao()
+
